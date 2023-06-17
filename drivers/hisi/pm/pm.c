2dc1fa4971aedd97ecdb6d3f0ad12a5de7cc6d8d
R0rt1z2 <me@r0rt1z2.com>
2023-06-17 13:39:03 +0000

    arch: arm64: configs: Disable Huawei DUBAI module
    Change-Id: I3d19cc9273645105b5834d23906bbf07d6415ad0

--- drivers/hisi/pm/pm.c
+++ drivers/hisi/pm/pm.c
@@ -149,8 +149,6 @@ void pm_gic_pending_dump(void)
 					power_monitor_report(WAKEUP_GPIO, "%d",
 							gpio);
                 }
-				/* notify dubai module to update wakeup information */
-				dubai_update_wakeup_info(g_ap_irq_name[irq], gpio);
 				printk("\n");
 			}
 		}
